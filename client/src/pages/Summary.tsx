604b6b39af544b0b4ca4a77f8998b71c789a7498
Robert Meredith <64670585+robertmeredith@users.noreply.github.com>
2024-10-24 15:05:20 +1100

    changed summary page to alter links to vendor and event pages

--- client/src/pages/Summary.tsx
+++ client/src/pages/Summary.tsx
@@ -11,7 +11,7 @@ import { SectionHeading } from '@/components'
 import { CalendarDays, CircleUserRound } from 'lucide-react'
 import { useGetUserEvents } from '@/hooks/eventQueries'
 import { useGetAllUserVendors } from '@/hooks/vendorQueries'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Summary() {
   const navigate = useNavigate()
@@ -32,21 +32,21 @@ function Summary() {
       />
       <div className="flex md:flex-row flex-col gap-4">
         <Card className="flex-1 p-1">
-          <CardHeader className="flex flex-row items-center gap-2 text-2xl font-semibold">
-            <CalendarDays />
-            Events
-            <Button
-              variant="light"
-              className="ml-auto"
-              onClick={() => navigate('/dashboard/events')}
+          <CardHeader className="py-4">
+            <Link
+              to="/dashboard/events"
+              className="flex flex-row items-center gap-2 text-2xl font-semibold pl-3"
             >
-              View Events
-            </Button>
+              <CalendarDays />
+              Events
+            </Link>
           </CardHeader>
           <Divider className="w-11/12 mx-auto" />
           <CardBody className="flex flex-col gap-2 mx-3">
             <p>Number of events:</p>
-            <p className="text-2xl font-bold">{events?.count}</p>
+            <Link to="/dashboard/events" className="max-w-max">
+              <p className="text-2xl font-bold">{events?.count}</p>
+            </Link>
           </CardBody>
           <Divider className="w-11/12 mx-auto" />
           <CardFooter className="flex gap-2">
@@ -62,21 +62,21 @@ function Summary() {
         </Card>
         {/* Vendors */}
         <Card className="flex-1">
-          <CardHeader className="flex flex-row items-center gap-2 text-2xl font-semibold">
-            <CircleUserRound />
-            Vendors
-            <Button
-              variant="light"
-              className="ml-auto"
-              onClick={() => navigate('/dashboard/vendors')}
+          <CardHeader className="py-4">
+            <Link
+              to="/dashboard/vendors"
+              className="flex flex-row items-center gap-2 text-2xl font-semibold pl-3"
             >
-              View Vendors
-            </Button>
+              <CircleUserRound />
+              Vendors
+            </Link>
           </CardHeader>
           <Divider className="w-11/12 mx-auto" />
           <CardBody className="flex flex-col gap-2 mx-3">
             <p>Number of vendors:</p>
-            <p className="text-2xl font-bold">{vendors?.count}</p>
+            <Link to="/dashboard/vendors" className="max-w-max">
+              <h1 className="text-2xl font-bold">{vendors?.count}</h1>
+            </Link>
           </CardBody>
           <Divider className="w-11/12 mx-auto" />
           <CardFooter className="flex flex-row justify-between">
